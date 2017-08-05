aedf916fe7942517c40e35126d7f749d36123978
Angel Tejeida <aetejeida@gmail.com>
2017-08-05 03:29:17 -0500

    web demo codiarq
    web demo codiarq

---
+++ assents/js/app.js
@@ -0,0 +1,221 @@
+﻿$(document).ready(function () {
+    $('.parallax').parallax();
+});
+
+
+
+function myScroll(value) {
+    $("html, body").animate({ scrollTop: $('#' + value + '').offset().top }, 1000);
+
+}
+
+function initMap() {
+    var uluru = { lat: 25.542054, lng: -103.441809 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru,
+        disableDefaultUI: true,
+        scrollwheel: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        styles: [
+  {
+      "elementType": "geometry",
+      "stylers": [
+        {
+            "color": "#242f3e"
+        }
+      ]
+  },
+  {
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+            "color": "#746855"
+        }
+      ]
+  },
+  {
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+            "color": "#242f3e"
+        }
+      ]
+  },
+  {
+      "featureType": "administrative",
+      "elementType": "geometry",
+      "stylers": [
+        {
+            "visibility": "off"
+        }
+      ]
+  },
+  {
+      "featureType": "administrative.locality",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+            "color": "#d59563"
+        }
+      ]
+  },
+  {
+      "featureType": "poi",
+      "stylers": [
+        {
+            "visibility": "off"
+        }
+      ]
+  },
+  {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+            "color": "#d59563"
+        }
+      ]
+  },
+  {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+        {
+            "color": "#263c3f"
+        }
+      ]
+  },
+  {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+            "color": "#6b9a76"
+        }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [
+        {
+            "color": "#38414e"
+        }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+            "color": "#212a37"
+        }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+            "visibility": "off"
+        }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+            "color": "#9ca5b3"
+        }
+      ]
+  },
+  {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        {
+            "color": "#746855"
+        }
+      ]
+  },
+  {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+            "color": "#1f2835"
+        }
+      ]
+  },
+  {
+      "featureType": "road.highway",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+            "color": "#f3d19c"
+        }
+      ]
+  },
+  {
+      "featureType": "transit",
+      "stylers": [
+        {
+            "visibility": "off"
+        }
+      ]
+  },
+  {
+      "featureType": "transit",
+      "elementType": "geometry",
+      "stylers": [
+        {
+            "color": "#2f3948"
+        }
+      ]
+  },
+  {
+      "featureType": "transit.station",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+            "color": "#d59563"
+        }
+      ]
+  },
+  {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+            "color": "#17263c"
+        }
+      ]
+  },
+  {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+            "color": "#515c6d"
+        }
+      ]
+  },
+  {
+      "featureType": "water",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+            "color": "#17263c"
+        }
+      ]
+  }
+        ]
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
